e2e7efd43044415d2c1c8c1c6fbcc6a059100e93
Bryce Johnson <brycepj@gmail.com>
2016-03-01 15:21:41 -0500

    Beautify JS and convert to spaced tabs.

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ var cfg = require('./lib/cfg'), paths = cfg.paths, http = cfg.http;
 var io = require('./lib/io');
 
 var bios = fs.readJsonSync(paths.poynterSrcJson)
-	.map(function(handle){
-		return { 
-			url: http.twitterBase + handle,
-			handle: handle
-		};
-	});
-	
+  .map(function (handle) {
+    return {
+      url: http.twitterBase + handle,
+      handle: handle
+    };
+  });
+
 io.makeRequests(bios)
-	.then(function(data){
-		fs.writeJson(paths.poynterDistJson, data);
-		console.log('JSON Written');
-		return data;
-	})
-	.then(function(data) {
-		console.log("Attempting writeXls");
-		io.writeXls(paths.poynterDistXls, data);
-		console.log('Done');
-	}).catch(function(err){ throw Error(err)});
+  .then(function (data) {
+    fs.writeJson(paths.poynterDistJson, data);
+    console.log('JSON Written');
+    return data;
+  })
+  .then(function (data) {
+    console.log("Attempting writeXls");
+    io.writeXls(paths.poynterDistXls, data);
+    console.log('Done');
+  }).catch(function (err) { throw Error(err) });
 
 
 
